db38a7ff4f5333e880c12e35e05e3682a9113108
RamKuppagiri <kramfl5@gmail.com>
2025-12-25 19:20:30 -0600

    queueDesign

---
+++ queueDesignUsingStacks.js
@@ -0,0 +1,53 @@
+
+class MyQueue {
+    constructor() {
+        // Not allocating space as the main way to test whether it is empty or not is by checking it's length. If we allocate the space, the length always holds some value and even though there is no data.
+        this.inStack = [];
+        this.outStack = [];
+    }
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+    this.inStack.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+    this.peek();
+    return this.outStack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+     if (!this.outStack.length) {
+        while (this.inStack.length > 0) {
+            this.outStack.push(this.inStack.pop());
+        }
+    }
+    if(!this.outStack.length) return;
+    return this.outStack[this.outStack.length - 1];
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+    return this.outStack.length === 0 && this.inStack.length === 0;
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
